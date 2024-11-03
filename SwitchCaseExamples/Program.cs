36f608fc9db27e0539a224c6eb09cf28644d328e
Savaş Tanrıverdi <savas.tanriverdi@outlook.com>
2024-11-03 22:24:43 +0300

    SwitchCaseExamples
    This project is prepared to learn the use of switch-case structure in C# language. The switch-case structure allows different code blocks to be executed according to the value of a given variable and provides a more organised structure instead of a large number of if-else statements. In this example, different operations are performed depending on a number or character.

---
+++ SwitchCaseExamples/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace SwitchCaseExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan bir gün numarası alarak haftanın gününü yazdırma
+            Console.Write("Enter a number (1-7) to get the day of the week: ");
+            int dayNumber = int.Parse(Console.ReadLine());
+
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number. Please enter a number between 1 and 7.");
+                    break;
+            }
+
+            // Kullanıcıdan bir karakter alarak basit bir işlem gerçekleştirme
+            Console.Write("\nEnter an operator (+, -, *, /): ");
+            char operation = Console.ReadLine()[0];
+
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine($"Result: {num1 + num2}");
+                    break;
+                case '-':
+                    Console.WriteLine($"Result: {num1 - num2}");
+                    break;
+                case '*':
+                    Console.WriteLine($"Result: {num1 * num2}");
+                    break;
+                case '/':
+                    if (num2 != 0)
+                        Console.WriteLine($"Result: {num1 / num2}");
+                    else
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator. Please enter one of +, -, *, /.");
+                    break;
+            }
+        }
+    }
+}
